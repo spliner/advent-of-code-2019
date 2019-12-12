5d2558e9d6a552d311d1c2aeaf4a4fd0d2088e50
Fillipe Rosini <hi@frosini.dev>
2019-12-11 23:34:59 -0300

    Finished day 6, part 1 in Rust

--- rust/day6/src/lib.rs
+++ rust/day6/src/lib.rs
@@ -1,28 +1,8 @@
-use crate::config::Config;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 
 pub mod config;
-
-#[derive(Debug)]
-struct Object {
-    name: String,
-    children: Vec<Box<&'static Object>>,
-}
-
-impl Object {
-    fn new(name: String) -> Self {
-        Self {
-            name,
-            children: Vec::new(),
-        }
-    }
-
-    fn add_child(&mut self, child: &'static Object) {
-        self.children.push(Box::new(child));
-    }
-}
 
 pub fn run(config: config::Config) -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string(config.filename)?;
@@ -41,7 +21,7 @@ pub fn run(config: config::Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn parse_orbits(input: &str) -> Vec<(String, String)> {
+fn parse_orbits(input: &str) -> HashMap<String, Vec<String>> {
     let orbits = input
         .lines()
         .map(|s| {
@@ -51,43 +31,35 @@ fn parse_orbits(input: &str) -> Vec<(String, String)> {
         .collect::<Vec<(&str, &str)>>();
 
     let mut node_map = HashMap::new();
-    for (parent_name, child_name) in orbits {
-        let child = node_map
-            .entry(String::from(child_name))
-            .or_insert(Object::new(String::from(child_name)));
+    orbits.iter()
+        .for_each(|(p, c)| {
+            let parent_name = String::from(*p);
+            let child_name = String::from(*c);
+            let parent = node_map.entry(parent_name).or_insert(Vec::new());
 
-//        let parent = node_map
-//            .entry(String::from(parent_name))
-//            .or_insert(Object::new(String::from(parent_name)));
-//        parent.add_child(child);
-    }
+            parent.push(child_name.clone());
+        });
 
-    input
-        .lines()
-        .map(|s| {
-            let orbit = s.split(")").map(|x| x.trim()).collect::<Vec<&str>>();
-            (String::from(orbit[0]), String::from(orbit[1]))
-        })
-        .collect::<Vec<(String, String)>>()
+    node_map
 }
 
-fn part_one(orbits: &Vec<(String, String)>) -> usize {
-    let mut orbit_map = HashMap::new();
+fn part_one(orbits: &HashMap<String, Vec<String>>) -> usize {
+    part_one_rec(&String::from("COM"), 0, orbits)
+}
 
-    for (parent, child) in orbits {
-        let children = orbit_map.entry(parent).or_insert(Vec::new());
-        children.push(child);
+fn part_one_rec(parent_name: &String, current_distance: usize, orbits: &HashMap<String, Vec<String>>) -> usize {
+    let children = orbits.get(parent_name);
+    match children {
+        Some(children) if children.len() > 0 => {
+            let mut total = 0;
+            for c in children {
+                total += part_one_rec(c, current_distance + 1, orbits);
+            }
+
+            total + current_distance
+        },
+        _ => current_distance
     }
-
-    let mut level = 0;
-    let mut count = 1;
-    for (parent, children) in orbit_map {
-        println!("{} ({}) => {:?}", parent, level, children);
-        count += level + children.len() + 1;
-        level += 1;
-    }
-
-    count
 }
 
 #[cfg(test)]
