86fd1625715ab63d9d9916486f1b1f06554fca7d
Fillipe Rosini <hi@frosini.dev>
2019-12-25 15:59:19 -0300

    Day 10 preparations in Rust

---
+++ rust/day10/src/lib.rs
@@ -0,0 +1,24 @@
+use std::error::Error;
+use std::fs;
+
+pub mod config;
+
+pub fn run(config: config::Config) -> Result<(), Box<dyn Error>> {
+    let _input = fs::read_to_string(config.filename)?;
+
+    match config.part {
+        config::Part::PartOne => {
+            // TODO: Part one
+        }
+        config::Part::PartTwo => {
+            // TODO: Part two
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
---
+++ rust/day10/src/main.rs
@@ -0,0 +1,17 @@
+use std::{env, process};
+
+use day10::config;
+
+fn main() {
+    let config = config::Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Error parsing arguments: {}", err);
+
+        process::exit(1);
+    });
+
+    if let Err(e) = day10::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
